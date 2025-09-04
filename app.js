8409712fbdd08289f19421478a7dec3f894f5673
emanuel9263 <emanuel9263@gmail.com>
2025-09-04 10:40:09 -0300

    implementacion de la funcion sortearAmigo, se selecciona un nombre al azar con Math.random() y se muestra el resultado

--- app.js
+++ app.js
@@ -25,4 +25,35 @@ function agregarAmigo(){
 
     console.log("Lista de amigos ", amigos);
 
+    actualizarLista();
+}
+
+function actualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+
+        const li = document.createElement("li");
+        
+        li.textContent = amigos[i];
+
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    // se valida que la lsita no se encuentre vacia
+    if (amigos.length === 0) {
+        alert("La lista de amigos esta vacia..,");
+        return
+    }
+    //se genera un indice aleatorio 
+    const indice = Math.floor(Math.random() * amigos.length);
+    //se selecciona el nombre correspondiente
+    const seleccionado = amigos[indice];
+    //se muestra el resultado en el elemento id = "resultado"
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>El amigo secreto es: <strong>${seleccionado}</strong></li>`;
+    
 }
